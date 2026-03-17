00dc9fd7c219d2f948e112c182478b61ce6dc828
lysbrodya <lysbrodya@gmail.com>
2026-03-17 13:53:01 +0200

    name langtha

--- client/src/js/api/api.js
+++ client/src/js/api/api.js
@@ -1,8 +1,7 @@
-// const API_URL = window.location.hostname === "localhost"
-//   ? "http://localhost:3000/api"
-//   : "/api";
+const API =
+  window.location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 // const API = "http://localhost:3000";
-const API = "/api";
+// const API = "/api";
 
 export const getStudents = () => fetch(`${API}/results`).then((r) => r.json());
 
