ff0259c6081d875e47c5473ffa37fa82a7dc7794
lysbrodya <lysbrodya@gmail.com>
2026-03-23 01:56:19 +0200

    add cron fix

--- api/cron.js
+++ api/cron.js
@@ -1,5 +1,5 @@
 import { supabase } from "./db.js";
-import { getResults } from "./results.js"; // твоя функция из Notion
+import { getResults } from "./notion.js"; // твоя функция из Notion
 
 export default async function handler(req, res) {
   try {
