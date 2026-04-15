676a78588f8b6421baa2f49d69f1b1ed98ab0659
lysbrodya <lysbrodya@gmail.com>
2026-04-15 18:51:02 +0300

    n8n cors fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       "/api": {
         target: "https://n8n.artosvita.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/webhook-test"),
+        rewrite: (path) => path.replace(/^\/api/, "/webhook"),
       },
     },
   },
